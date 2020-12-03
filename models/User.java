b1cb20e540fb09f249244d8c4028f2f63c385480
Aniket Konkar <inspiration601@gmail.com>
2020-12-03 09:18:01 +0530

    added serialVersionUID for user & airport model

--- models/User.java
+++ models/User.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
